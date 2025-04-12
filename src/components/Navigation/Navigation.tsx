445db53b363ddc2ecaaf8a5ef4829b7fa1792366
Jorge Matos <jisapjisap@gmail.com>
2025-04-12 13:28:10 +0200

    feat: enhance Header and Navigation components with responsive styles and Hamburger icon

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -4,23 +4,26 @@ import CaretUp from "../Icons/CaretUp"
 import LogoIcon from "../Icons/LogoIcon"
 import SearchIcon from "../Icons/SearchIcon"
 import DestopkDropdown from "./DestopkDropdown"
+import Hamburger from "../Icons/Hamburger.tsx";
 
 
 const Navigation = () => {
   return (
-    <nav className="m-auto mb-12 max-w-432">
-      <div className="flex items-center justify-between px-21.5">
-        <a href="/" className="flex items-center gap-x-2.5">
-          <LogoIcon className="size-13" />
-          <span className="text-3xl font-semibold">GoTravel</span>
+    <nav className="max-3xl:mb-10.5 m-auto mb-12 max-w-432 max-xl:mb-9 max-sm:mb-16">
+      <div className="flex items-center justify-between px-21.5 max-3xl:px-16 max-2xl:px-9 max-xl:px-4 max-lg:px-0">
+        <a href="/" className="flex items-center gap-x-2.5 max-sm:gap-x-2">
+          <LogoIcon className="size-13 max-xl:size-10.5 max-lg:size-9" />
+          <span className="text-3xl font-semibold max-2xl:text-2xl max-xl:text-xl max-lg:text-lg">
+            GoTravel
+          </span>
         </a>
 
-        <ul className="flex items-stretch gap-x-10">
+        <ul className="flex items-stretch gap-x-10 max-2xl:gap-x-8 max-xl:gap-x-6 max-lg:hidden">
           {navigationLinks.map((link) => (
             <li key={link.id} className="group relative flex cursor-pointer items-center gap-x-1.75 font-medium transition-all duration-200">
               <a 
                 href={link.href} 
-                className="text-grey-600 group-hover:text-black"
+                className="text-grey-600 group-hover:text-black max-2xl:text-[.9375rem] max-xl:text-[.875rem]"
               >
                 {link.text}
               </a>
@@ -36,11 +39,15 @@ const Navigation = () => {
           ))}
         </ul>
 
-        <div>
-          <BellIcon className=""/>
-          <SearchIcon className=""/>
-          <button>Join Now</button>
+        <div className="flex items-center gap-x-5 max-xl:gap-x-4 max-lg:hidden">
+          <BellIcon className="fill-grey-600 cursor-pointer hover:fill-black"/>
+          <SearchIcon className="fill-grey-600 cursor-pointer hover:fill-black"/>
+          <button className="bg-primary-700 hover:bg-primary-800 cursor-pointer rounded-[0.625rem] px-8 py-3.5 text-lg font-medium text-white transition-all duration-200 max-xl:py-3 max-xl:text-sm max-xl:font-normal">
+            Join Now
+          </button>
         </div>
+
+        <Hamburger />
       </div>
     </nav>
   )
