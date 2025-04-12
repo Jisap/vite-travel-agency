d1298fa4cfed5d6a6d221e871c9725561b30b121
Jorge Matos <jisapjisap@gmail.com>
2025-04-12 12:59:28 +0200

    refactor: improve Navigation component structure and styling

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -8,27 +8,30 @@ import DestopkDropdown from "./DestopkDropdown"
 
 const Navigation = () => {
   return (
-    <nav>
-      <div className="flex items-center justify-between">
-        <a href="/">
-          <LogoIcon className="h-10 w-10" />
-          <span>GoTravel</span>
+    <nav className="m-auto mb-12 max-w-432">
+      <div className="flex items-center justify-between px-21.5">
+        <a href="/" className="flex items-center gap-x-2.5">
+          <LogoIcon className="size-13" />
+          <span className="text-3xl font-semibold">GoTravel</span>
         </a>
 
-        <ul className="flex items-center gap-4">
-          {navigationLinks.map((link, index) => (
-            <li key={index}>
-              <a href={link.href}>
+        <ul className="flex items-stretch gap-x-10">
+          {navigationLinks.map((link) => (
+            <li key={link.id} className="group relative flex cursor-pointer items-center gap-x-1.75 font-medium transition-all duration-200">
+              <a 
+                href={link.href} 
+                className="text-grey-600 group-hover:text-black"
+              >
                 {link.text}
               </a>
               {link.dropdown && (
-                <span>
-                  <CaretUp className="" />
+                <span className="flex size-6 items-center justify-center">
+                  <CaretUp className="size-3.5 fill-grey-600 rotate-180 transition-all duration-250 group-hover:fill-black group-hover:rotate-360"  />
                 </span>
               )}
-              {link.dropdown && (
+              {/* {link.dropdown && (
                 <DestopkDropdown links={link.dropdownLinks} />
-              )}
+              )} */}
             </li>
           ))}
         </ul>
