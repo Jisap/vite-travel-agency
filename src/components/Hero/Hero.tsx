5a1a93dacbe66a880de70c196987cbe7a94f1134
Jorge Matos <jisapjisap@gmail.com>
2025-04-13 13:27:46 +0200

    refactor: update Hero component structure for improved layout and styling

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -8,9 +8,9 @@ const Hero = () => {
     <section className="relative m-auto max-w-[108rem]">
       <div className="relative flex items-center justify-center bg-[url('/public/hero.webp')] rounded-[2.5rem] bg-cover bg-bottom pb-[56.25%]">
 
-        <div>
+        <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center text-center">
           <div>
-            <h1>Star Your Memorable <br /> Journey Here</h1>
+            <h1 className="mb-4 pt-54 text-[6.5rem]/34 font-semibold text-white">Star Your Memorable <br /> Journey Here</h1>
             <p>
               Where unforgettable adventures await you.
             </p>
