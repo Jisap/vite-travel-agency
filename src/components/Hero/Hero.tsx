74b61f984270dd8c994a3cb3291b7256f8c4ce8e
Jorge Matos <jisapjisap@gmail.com>
2025-04-13 13:19:02 +0200

    refactor: enhance Hero component layout with responsive styles and background image

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,8 +5,8 @@ import HeroBenefit from "./HeroBenefit"
 
 const Hero = () => {
   return (
-    <section>
-      <div>
+    <section className="relative m-auto max-w-[108rem]">
+      <div className="relative flex items-center justify-center bg-[url('/public/hero.webp')] rounded-[2.5rem] bg-cover bg-bottom pb-[56.25%]">
 
         <div>
           <div>
