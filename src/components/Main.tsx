3476b3f446a5daf5c729b4d14348074d43717485
Jorge Matos <jisapjisap@gmail.com>
2025-04-14 16:00:21 +0200

    main component

---
+++ src/components/Main.tsx
@@ -0,0 +1,16 @@
+import { ReactElement } from "react"
+
+
+interface MainProps {
+  children: ReactElement;
+}
+
+const Main = (props: MainProps) => {
+  return (
+    <main>
+      {props.children}
+    </main>
+  )
+}
+
+export default Main
