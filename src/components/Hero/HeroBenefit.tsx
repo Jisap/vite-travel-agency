a28651f74cbd4dff936c3d96af553ece95e9a38c
Jorge Matos <jisapjisap@gmail.com>
2025-04-14 15:10:21 +0200

    Hero

--- src/components/Hero/HeroBenefit.tsx
+++ src/components/Hero/HeroBenefit.tsx
@@ -7,7 +7,20 @@ interface HeroBenefitProps {
 
 const HeroBenefit = ({ benefit }: HeroBenefitProps) => {
   return (
-    <li>{benefit.heading}</li>
+    <li key={benefit.id} className="flex items-center gap-x-12">
+      <div className="bg-primary-100 flex size-24 items-center justify-center rounded-full">
+        <benefit.Icon className="fill-primary-700"/>
+      </div>
+
+      <div>
+        <p className="text-grey-600 tracking-6 text-[1.75rem]/10.5 font-medium whitespace-nowrap">
+          {benefit.heading}
+        </p>
+        <p className="tracking-6 text-grey-600 font-rubik text-base/7.5">
+          {benefit.description}
+        </p>
+      </div>
+    </li>
   )
 }
 
