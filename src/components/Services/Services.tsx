3220e64841ac1054be8dbfc2852d0566432bd1b5
Jorge Matos <jisapjisap@gmail.com>
2025-04-15 16:37:35 +0200

    service responsive

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -8,12 +8,21 @@ const destinationExpertise: IService = services.at(2)!;
 
 const Services = () => {
   return (
-    <section className="bg-primary-100 px-24 py-36">
-      <ul className="border-grey-500/40 m-auto flex max-w-389 justify-between border-y-1 py-24">
+    <section className="bg-primary-100 px-24 py-36 
+      max-3xl:py-34 max-3xl:px-20 max-2xl:px-14 max-2xl:py-28 
+      max-xl:px-10 max-xl:py-26 
+      max-lg:px-6 max-lg:py-24 
+      max-sm:px-4"
+    >
+      <ul className="border-grey-500/40 m-auto flex max-w-389 justify-between border-y-1 py-24 
+        max-3xl:py-23 max-2xl:py-20 max-xl:gap-x-10 max-xl:py-16 
+        max-lg:flex-wrap max-lg:justify-center max-lg:gap-y-12 max-lg:border-none max-lg:py-0 
+        max-md:gap-y-16"
+      >
         <Service service={customizedPackages} />
-        <li className="bg-gray-500/40 block w-0.25 self-stretch"/>
+        <li className="bg-gray-500/40 block w-0.25 self-stretch max-lg:hidden"/>
         <Service service={culinaryTours} />
-        <li className="bg-gray-500/40 block w-0.25 self-stretch"/>
+        <li className="bg-gray-500/40 block w-0.25 self-stretch max-lg:hidden"/>
         <Service service={destinationExpertise} />
       </ul>
     </section>
