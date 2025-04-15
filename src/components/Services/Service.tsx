8d02bef6ce30ba54e4a13336fe1f410dd067176c
Jorge Matos <jisapjisap@gmail.com>
2025-04-15 16:18:06 +0200

    Services

--- src/components/Services/Service.tsx
+++ src/components/Services/Service.tsx
@@ -7,11 +7,13 @@ interface ServiceProps{
 
 const Service = ({service}: ServiceProps) => {
   return (
-    <li>
-      <service.Icon className="h-26" />
-      <div>
-        <h3>{service.heading}</h3>
-        <p>{service.description}</p>
+    <li key={service.id} className="h-90 flex flex-col justify-between">
+      <service.Icon className="h-26 place-self-center" />
+      <div className="text-center">
+        <h3 className="tracking-6 mb-9.5 text-[1.75rem]/10.5 font-semibold text-nowrap">
+          {service.heading}
+        </h3>
+        <p className="text-grey-900/85 tracking-6 font-rubik text-base/9.5">{service.description}</p>
       </div>
     </li>
   )
