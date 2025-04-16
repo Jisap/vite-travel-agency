aab8d4b53a1067e636cd49476f5ea21ce69c119d
Jorge Matos <jisapjisap@gmail.com>
2025-04-16 15:28:32 +0200

    LocationCard

--- src/components/ExploreMore/LocationCard.tsx
+++ src/components/ExploreMore/LocationCard.tsx
@@ -1,4 +1,6 @@
 import { type Location } from "../../utils/contentTypes"
+import { Location as LocationIcon } from "../Icons/Location"
+import Star from "../Icons/Star"
 
 interface LocationCardProps {
   location: Location
@@ -6,7 +8,27 @@ interface LocationCardProps {
 
 const LocationCard = ({location}: LocationCardProps) => {
   return (
-    <div>LocationCard</div>
+    <li>
+      <div>
+        <img src={location.img} alt={location.alt} />
+        <div>
+          <Star className="size-6" />
+          <p>{location.rating}</p>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <p>{location.title}</p>
+          <div>
+            <LocationIcon className="size-6" />
+            <p>{location.location}</p>
+          </div>
+        </div>
+
+        <p>{location.pricePerPerson}/<span>Pax</span></p>
+      </div>
+    </li>
   )
 }
 
