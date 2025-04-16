5bb8d15e37a7afb5c2e2713968e5b444c3d311af
Jorge Matos <jisapjisap@gmail.com>
2025-04-16 17:59:08 +0200

    exploreMore responsive

--- src/components/ExploreMore/ExploreMore.tsx
+++ src/components/ExploreMore/ExploreMore.tsx
@@ -21,22 +21,31 @@ const ExploreMore = () => {
   }
 
   return (
-    <section className="px-24 py-26">
-      <div className="m-auto max-w-389">
-        <div className="flex items-end justify-between">
+    <section className="px-24 py-26
+      max-3xl:px-20 max-2xl:px-14 max-xl:px-10 max-lg:px-6 max-lg:py-24 max-sm:px-4"
+    >
+      {/* En pantallas de 800px o menos (≤ 50rem), el elemento tendrá un ancho máximo de 32rem (512px)*/}
+      <div className="m-auto max-w-389 max-mdlg:max-w-lg">
+        <div className="flex items-end justify-between max-sm:flex-wrap max-sm:gap-x-16 max-sm:gap-y-10">
           <div>
-            <h2 className="tracking-6 mb-4 text-[3.25rem] font-semibold">
+            <h2 className="tracking-6 mb-4 text-[3.25rem] font-semibold
+              max-3xl:text-[2.875rem] max-2xl:text-[2.625rem] max-xl:text-[2.25rem] max-md:mb-3 max-sm:text-[2rem]"
+            >
               Explore more
             </h2>
-            <p className="tracking-6 text-grey-700 text-[1.75rem] font-light">
+            <p className="tracking-6 text-grey-700 text-[1.75rem] font-light
+              max-3xl:text-[1.5rem] max-2xl:text-[1.375rem] max-xl:text-[1.25rem] max-md:text-lg max-sm:text-base"
+            >
               Let&apos;s go on adventure
             </p>
           </div>
 
-          <div className="mb-2 flex gap-x-6">
+          <div className="mb-2 flex gap-x-6
+            max-3xl:mb-1.5 max-3xl:gap-x-5 max-2xl:gap-x-4 max-xl:mb-0.5 max-sm:gap-x-3"
+          >
             <button 
               className="bg-grey-300 not-disabled:hover:bg-grey-400 flex size-18 cursor-pointer place-content-center rounded-full
-              transition-all duration-200 disabled:cursor-not-allowed disabled:opacity-50"
+              transition-all duration-200 disabled:cursor-not-allowed disabled:opacity-50 max-2xl:size-17 max-xl:size-16 max-sm:size-15"
               aria-label="arrow-left"
               onClick={handleLeftClick}
               disabled={currentIndex === 0}
@@ -45,7 +54,7 @@ const ExploreMore = () => {
             </button>
             <button 
               className="bg-primary-700 not-disabled:hover:bg-primary-800 flex size-18 cursor-pointer place-content-center rounded-full
-              transition-all duration-200 disabled:cursor-not-allowed disabled:opacity-50"
+              transition-all duration-200 disabled:cursor-not-allowed disabled:opacity-50 max-2xl:size-17 max-xl:size-16 max-sm:size-15"
               aria-label="arrow-right"
               onClick={handleRightClick}  
               disabled={currentIndex >= totalLocations - 6}
@@ -55,7 +64,12 @@ const ExploreMore = () => {
           </div>
         </div>
 
-        <ul className="mt-33 grid grid-cols-3 gap-x-29 gap-y-24">
+        <ul className="mt-33 grid grid-cols-3 gap-x-29 gap-y-24
+          max-3xl:mt-31 max-2xl:mt-24 max-xl:mt-20 max-md:mt-16
+          max-3xl:gap-x-26 max-2xl:gap-x-16 max-xl:gap-x-22 max-lg:gap-x-12
+          max-2xl:gap-y-20 max-sm:gap-y-16
+          max-mdlg:grid-cols-1 max-xl:grid-cols-2   "
+        >
           {renderLocations?.map((location) => (
             <LocationCard 
               location={location} 
--- src/components/ExploreMore/LocationCard.tsx
+++ src/components/ExploreMore/LocationCard.tsx
@@ -14,30 +14,40 @@ const LocationCard = ({location}: LocationCardProps) => {
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: 20 }}
       transition={{ duration: 0.35, ease: "easeInOut" }}
-      className="group cursor-pointer">
-      <div className="relative mb-8 overflow-hidden rounded-3xl">
+      className="group cursor-pointer"
+    >
+      <div className="relative mb-8 overflow-hidden rounded-3xl max-3xl:mb-7.5 max-sm:mb-6.5">
         <img 
           src={location.img} 
           alt={location.alt} 
-          className="transform transition-all duration-300 ease-in-out group-hover:scale-103"  
+          className="transform transition-all duration-300 ease-in-out group-hover:scale-103
+          max-mdlg:aspect-[361/327]"  
         />
-        <div className="absolute top-5 right-6 flex items-center gap-x-2 rounded-[.625rem] bg-white/85 backdrop-blur-3xl px-3 py-1.5">
-          <Star className="size-6 fill-yellow" />
-          <p className="text-grey-600 tracking-6 text-[1.0625rem] font-semibold">{location.rating}</p>
+        <div className="absolute top-5 right-6 flex items-center gap-x-2 rounded-[.625rem] bg-white/85 backdrop-blur-3xl px-3 py-1.5
+          max-3xl:gap-x-1.5 max-sm:top-4.5 max-sm:right-4.5"
+        >
+          <Star className="size-6 fill-yellow max-2xl:size-5" />
+          <p className="text-grey-600 tracking-6 text-[1.0625rem] font-semibold max-3xl:text-base max-2xl:text-sm">
+            {location.rating}
+          </p>
         </div>
       </div>
 
-      <div className="flex items-end justify-between">
+      <div className="flex items-end justify-between max-sm:items-start">
         <div>
-          <p className="mb-3.5 text-[1.75rem] font-semibold">{location.title}</p>
+          <p className="mb-3.5 text-[1.75rem] font-semibold max-3xl:text-[1.5rem] max-3xl:mb-3">
+            {location.title}
+          </p>
           <div className="flex items-center">
-            <LocationIcon className="size-6 mr-1.5" />
-            <p className="text-grey-700 text-lg">{location.location}</p>
+            <LocationIcon className="size-6 mr-1.5 max-3xl:size-5 max-3xl:mr-1" />
+            <p className="text-grey-700 text-lg max-3xl:text-base">
+              {location.location}
+            </p>
           </div>
         </div>
 
-        <p className="font-rubik text-[1.75rem]">
-          ${location.pricePerPerson}/<span className="text-[1.25rem]">Pax</span>
+        <p className="font-rubik text-[1.75rem] max-3xl:text-[1.5rem]">
+          ${location.pricePerPerson}/<span className="text-[1.25rem] max-3xl:text-[1.0625rem]">Pax</span>
         </p>
       </div>
     </motion.li>
