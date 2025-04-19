a05f632bac35819c38d88daed1f1f980728040a4
Jorge Matos <jisapjisap@gmail.com>
2025-04-19 17:17:19 +0200

    Basic Tesmimonials styles

--- src/components/Testimonials/Testimonial.tsx
+++ src/components/Testimonials/Testimonial.tsx
@@ -6,14 +6,23 @@ interface TestimonialProps {
 
 const Testimonial = ({ testimonial }: TestimonialProps) => {
   return (
-    <li>
-      <div>
-        <img src={testimonial.img} alt={testimonial.alt} /> 
+    <li className="relative ml-24 flex rounded-3xl bg-white py-14 pt-14 pr-12 pl-42 drop-shadow-[0px_0px_20px_rgba(0,0,0,0.05)]">
+      <div className="absolute left-0 -translate-[50%] size-47.5 overflow-hidden 
+      after:absolute after:right-0 after:bottom-0 after:-z-1 after:block after:size-45 after:rounded-full after:bg-primary-700">
+        <img 
+          src={testimonial.img} 
+          alt={testimonial.alt} 
+          className="bg-grey-300 top-0 left-0 z-5 size-45 rounded-full"  
+        /> 
       </div>
 
-      <div>
-        <p>{testimonial.description}</p>
-        <p>{testimonial.name} / {testimonial.vacation}</p>
+      <div className="flex flex-col justify-between gap-y-14">
+        <p className="text-base/9 font-light">
+          {testimonial.description}
+        </p>
+        <p className="font-medium">
+          {testimonial.name} /{" "}<span className="font-light">{testimonial.vacation}</span>
+        </p>
       </div>
     </li>
   )
