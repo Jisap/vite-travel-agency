94a22951289f5c702bbdea3cc6db3966bac2a46f
Jorge Matos <jisapjisap@gmail.com>
2025-04-20 11:55:07 +0200

    Footer basic styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,22 +4,24 @@ import LogoIconDark from "./Icons/LogoIconDark"
 
 const Footer = () => {
   return (
-    <section>
-      <div>
+    <footer className="bg-primary-700 px-24 pt-24 pb-26">
+      <div className="m-auto flex  max-w-432 justify-between gap-x-32 text-white">
         {/* left */}
-        <div>
-          <LogoIconDark className="" />
+        <div className="flex max-w-116 gap-x-4">
+          <LogoIconDark className="block shrink-0" />
 
           <div>
-            <p>GoTravel - Your Vacation Awaits</p>
+            <p className="mb-5 pt-3.5 text-[1.25rem] font-semibold">
+              GoTravel - Your Vacation Awaits
+            </p>
 
-            <p>
+            <p className="mb-8 font-light">
               GoTravel creates vacations you&apos;ll remember. Simply pick your
               desired location, and we do the rest. An all inclusive getaway
               that leaves you relaxing, not stressing.
             </p>
 
-            <ul>
+            <ul className="flex gap-x-8">
               {footerSocials.map((social) => (
                 <li key={social.id}>
                   <a 
@@ -28,7 +30,7 @@ const Footer = () => {
                     target="_blank" 
                     className=""
                   >
-                    <social.Icon className="" />
+                    <social.Icon className="fill-white" />
                   </a>                
                 </li>
               ))}
@@ -37,15 +39,23 @@ const Footer = () => {
         </div>
 
         {/* right */}
-        <ul>
+        <ul className="flex basis-256.5 justify-between">
           {footerCols.map((col) => (
             <li key={col.id}>
-              <p>{col.heading}</p>
-              <ul>
+              <p className="mb-5 text-xl font-semibold">
+                {col.heading}
+              </p>
+              <ul className="flex flex-col gap-y-2.5">
                 {col.links.map((link) => (
-                  <li key={link.id}>
-                    {link.Icon && <link.Icon className="" />}
-                    <a href={link.href} className="">
+                  <li 
+                    key={link.id} 
+                    className="flex items-center gap-x-2"
+                  >
+                    {link.Icon && <link.Icon className="block size-6 fill-white" />}
+                    <a 
+                      href={link.href} 
+                      className="font-medium opacity-80 hover:opacity-100"
+                    >
                       <p>
                         {link.name}
                       </p>
@@ -57,7 +67,7 @@ const Footer = () => {
           ))}
         </ul>
       </div>
-    </section>
+    </footer>
   )
 }
 
